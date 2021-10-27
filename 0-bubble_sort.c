a46a08397f40a1401b93aefe7d4f06d91533137d
aloicerains <aloiceokoth98@gmail.com>
2021-10-27 12:37:23 +0300

    changed <= to <

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,12 +9,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swapped;
 
-	if (array == NULL || size < 1)
+	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-		for (j = 0; j <= (size - 1 - i); j++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
